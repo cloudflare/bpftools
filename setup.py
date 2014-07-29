e72149fc4cdd35e5d77961661adc1bbeb12b0417
Marek Majkowski <marek@cloudflare.com>
2014-07-29 13:01:09 +0100

    Dummy setup.py to make setuptools happy

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='bpftools',
+      version='1.0',
+      description='BPF Tools - packet analyst toolkit',
+      url='https://github.com/cloudflare/bpftools',
+      packages=['bpftools'],
+      maintainer="Marek Majkowski",
+      maintainer_email="marek@cloudflare.com",
+     )
