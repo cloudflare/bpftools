a28bd27b0c6ff3b74464cefab50d4ad78eac1e0a
Geoffrey Plouviez <geoffrey@cloudflare.com>
2015-03-26 17:16:00 -0700

    So this is the badrand tool which is basically checking if a packet IP ID is the value of the byte swapped TCP/UDP port.
    Manually tested with 20 packets.

---
+++ bpftools/gen_dns_badrand.py
@@ -0,0 +1,36 @@
+import argparse
+import string
+import sys
+
+def gen(params, l3_off=0, ipversion=4, negate=False):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        prog="%s dns_badrand --" % (sys.argv[0]),
+        description=r''' 
+
+Generate raw BPF rules that match packets formed with predicitable (non)random fields.
+In some specific packets, the IP Identifier field could have the value of thebyte swaped 
+TCP/UDP Source port. This is what is detected by this BPF rule.
+
+''')
+
+
+    if ipversion == 4:
+        print "ldx 4*([%i]&0xf)" % (l3_off)     # IHL value
+        print "ldb [x + %i]" % (l3_off + 1)     # Loading TCP/UDP byte 1 port into A
+        print "lsh #8"                          # Bit shifting
+        print "st M[%i]" % (0)                  # Storing A into M[0]
+        print "ldb [x + %i]" % (l3_off)         # Loading TCP/UDP byte 0 port into A
+        print "ldx M[%i]" % (0)                 # Loading M[0] (UDP port shifted byte 1) into X
+        print "or x"                            # Oring A and X
+        print "tax"
+
+        print "ldh [%i]" % (l3_off + 4)         # Loading IP identifier into A
+        print "xor x"                           # Xoring A and X (Port and IP ID)
+        print "jeq #0x0, match" 
+        print "ret #%i" % (0 if not negate else 1)
+        print "match:"
+        print "ret #%i" % (1 if not negate else 0)
+
+    return ''
+
