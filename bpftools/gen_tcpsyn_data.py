3d634d3103e5582ed11e4cc8bb90004014791781
Geoffrey Plouviez <geoffrey@cloudflare.com>
2015-04-02 16:20:24 +0100

    ipv6 support. Requires more testing

--- bpftools/gen_tcpsyn_data.py
+++ bpftools/gen_tcpsyn_data.py
@@ -13,16 +13,18 @@ Ble
 ''')
 
     if ipversion == 6:
-        print "jmp nonmatch"
-        # for both ipv4 et ipv6 jai besoin de taille header IP et taille globale...
-        # loader ca dns m[0] et m[1]
-        # et de verifier que cest bien du TCP
-        
+        frame_size_off = 4
+        protocol_value_off = 6
+        print "ldx #40"      # No extension header support
+
     if ipversion == 4:
+        frame_size_off = 2
+        protocol_value_off = 9
         print "ldx 4*([%i]&0xf)" % (l3_off)  # IHL value in X
-        print "ldh [%i]" % (l3_off + 2)      # Global frame size in m[1]
-        print "st M[%i]" % (1)
-        print "ldb [%i]" % (l3_off + 9)      # Protocol value in A
+
+    print "ldh [%i]" % (l3_off + frame_size_off)      # Global frame size in m[1]
+    print "st M[1]" 
+    print "ldb [%i]" % (l3_off + protocol_value_off)      # Protocol value in A
 
     print "jneq #0x06, nonmatch" #0x06 is TCP protocol in IPV4/IPV6
 
@@ -34,16 +36,16 @@ Ble
 
     print "jneq #0x2, nonmatch" #Error if value is 0 or 00010000 or 00010010 
     
-    print "ld M[%i]" % (1)
-    print "sub x" #result in A
-    print "st M[%i]" % (3) #result in M[3]
+    print "ld M[1]" 
+    print "sub x"    #result in A
+    print "st M[3]" #result in M[3]
     
     print "ldb [x + %i]" % (l3_off + 12)
     print "rsh #0x4" # The TCP header size is stored on the 4 MSB...
     print "mul #0x4" # ...value which is stored in 32bits words... need the value in bytes in A
     
     #We do not need the value stored in X anymore can use it
-    print "ldx M[%i]" % (3)
+    print "ldx M[3]"
 
     print "sub x" #Should be 0
     print "jneq #0x0, nonmatch"
